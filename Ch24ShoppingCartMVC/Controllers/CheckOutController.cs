eb95f2e65170019706e7018fdad2db4e6ae00116
MALAV MISTRY <malav_mistry@yahoo.co.in>
2017-01-16 22:03:24 -0800

    HalloweenSuperStore

---
+++ Ch24ShoppingCartMVC/Controllers/CheckOutController.cs
@@ -0,0 +1,28 @@
+﻿using Ch24ShoppingCartMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ch24ShoppingCartMVC.Controllers
+{
+    public class CheckOutController : Controller
+    {
+        //
+        // GET: /CheckOut/
+
+        private CartModel cart = new CartModel();
+
+        public ActionResult Index()
+        {
+            CartViewModel model = (CartViewModel)TempData["cart"];
+            if (model == null)
+            {
+                model = cart.GetCart();
+            }
+            //Passing model to View
+            return View(model);
+        }
+    }
+}
